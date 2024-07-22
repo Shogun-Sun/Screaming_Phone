316e0c47e81947122f612c144dc3d2b98e947dea
Shogun_Sun <fima8516@gmail.com>
2024-07-22 17:15:46 +0300

    Separate MainActivity and add background work
    Separate `MainActivity.java` into `SensorHandler.java` and `Work.java`
    
    SensorHandler: Responsible for receive data from the Accelerometer sensor and handle it
    
    Work.java: Responsible for the work performed under specific conditions
    
    Adding background work

--- app/src/main/java/com/example/stats/MainActivity.java
+++ app/src/main/java/com/example/stats/MainActivity.java
@@ -1,34 +1,28 @@
 package com.example.stats;
 
-import android.app.Activity;
-import android.content.ComponentName;
-import android.content.Context;
-import android.content.Intent;
+
 import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.media.MediaPlayer;
-import android.os.Build;
 import android.os.Bundle;
 import android.widget.TextView;
-import android.app.Service;
 
-import com.example.stats.R;
 
-import java.util.List;
-public class MainActivity extends Activity implements SensorEventListener {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
 
     /** Объект типа сенсор менеджер */
     SensorManager mSensorManager;
     /** Создали объект типа сенсор для получения данных угла наклона телефона */
     Sensor mAccelerometerSensor;
     Sensor mMagneticFieldSensor;
-    /** Наши текствью в которые будем все выводить */
+    /** Вывод полученных данных*/
     TextView mXValueText;
     TextView mYValueText;
     TextView mZValueText;
     private MediaPlayer sirSound;
+    private SensorHandler SensorHandler;
 
     /** Called when the activity is first created. */
     @Override
@@ -36,79 +30,33 @@ public class MainActivity extends Activity implements SensorEventListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mXValueText = findViewById(R.id.value_x);
+        mYValueText = findViewById(R.id.value_y);
+        mZValueText = findViewById(R.id.value_z);
         sirSound = MediaPlayer.create(this, R.raw.sound);
-        // присвоили менеджеру работу с серсором
-        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        // создали список сенсоров для записи и сортировки
-        List<Sensor> sensors = mSensorManager.getSensorList(Sensor.TYPE_ALL);
-        // делаем проверку если больше нуля значит все хорошо и начинаем обрабатывать работу датчика
-        if (sensors.size() > 0) {
-            // форич для зацикливания работы, что бы не единожды выполнялось, а постоянно
-            for (Sensor sensor : sensors) {
-                // берем данные с акселерометра
-                switch (sensor.getType()) {
-                    case Sensor.TYPE_ACCELEROMETER:
-                        // если пусто значит возвращаем значения сенсора
-                        if (mAccelerometerSensor == null)
-                            mAccelerometerSensor = sensor;
-                        break;
-                    default:
-                        break;
-                }
-            }
-        }
-        // привязываем наши объекты к нашей разметке
-        mXValueText = (TextView) findViewById(R.id.value_x);
-        mYValueText = (TextView) findViewById(R.id.value_y);
-        mZValueText = (TextView) findViewById(R.id.value_z);
+        SensorHandler = new SensorHandler(this, mXValueText, mYValueText, mZValueText, sirSound);
+
+//        findViewById(R.id.value_x).setOnClickListener(v -> {
+//            Intent serviceIntent = new Intent(this, ForegroundService.class);
+//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//                startForegroundService(serviceIntent);
+//            } else {
+//                startService(serviceIntent);
+//            }
+//        });
+
     }
 
     @Override
     protected void onPause() {
-
-        //говорим что данные будем получать из этого класса
-        mSensorManager.unregisterListener(this);
         super.onPause();
+        SensorHandler.registerListener();
     }
 
     @Override
     protected void onResume() {
 
         super.onResume();
-        //регистрируем сенсоры в объекты сенсора
-        mSensorManager.registerListener(this, mAccelerometerSensor, SensorManager.SENSOR_DELAY_GAME);
-        mSensorManager.registerListener(this, mMagneticFieldSensor, SensorManager.SENSOR_DELAY_GAME);
-
-    }
-
-    @Override
-    public void onSensorChanged(SensorEvent event) {
-
-
-        //создали массив в которые будем записывать наши данные полученые с датчиков
-        float[] values = event.values;
-        switch (event.sensor.getType()) {
-            case Sensor.TYPE_ACCELEROMETER: {
-                //собственно выводим все полученые параметры в текствьюшки наши
-                mXValueText.setText(String.format("%1.3f", event.values[SensorManager.DATA_X]));
-                mYValueText.setText(String.format("%1.3f", event.values[SensorManager.DATA_Y]));
-                mZValueText.setText(String.format("%1.3f", event.values[SensorManager.DATA_Z]));
-            }
-            break;
-        }
-        if(event.values[SensorManager.DATA_Y] <= -15){
-            soundPlayButton(sirSound);
-        }
-
-
-    }
-
-    private void soundPlayButton(MediaPlayer sound) {
-        sound.start();
-    }
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
+        SensorHandler.registerListener();
     }
 }
--- app/src/main/java/com/example/stats/Work.java
+++ app/src/main/java/com/example/stats/Work.java
@@ -1,11 +1,9 @@
 package com.example.stats;
 
 import android.content.Context;
-import android.hardware.Sensor;
 import android.media.MediaPlayer;
 import android.os.Handler;
 import android.os.Looper;
-import android.util.Log;
 
 public class Work {
     private MediaPlayer sirSound;
